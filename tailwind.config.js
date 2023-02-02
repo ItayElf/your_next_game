b1fb5a0475ddf5524c0c548f55fcc7ab48b57d85
ItayElf <thh123321@gmail.com>
2023-02-02 18:07:40 +0200

    Feat: added default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,29 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#fdf3e1",
+          100: "#fbe1b3",
+          DEFAULT: "#f8cd82",
+          200: "#f8cd82",
+          300: "#f6b950",
+          400: "#f5aa2b",
+          500: "#f49c0b",
+          600: "#f09007",
+          accent: "#ea8103",
+          700: "#ea8103",
+          800: "#e47200",
+          900: "#db5a00",
+        },
+        error: "#cf6679",
+        surface: "#121212"
+      }
+    },
   },
   plugins: [require('prettier-plugin-tailwindcss')],
 }
