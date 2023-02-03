4b647142141c1263f4bd3fb649d77a53f4029519
ItayElf <thh123321@gmail.com>
2023-02-03 18:31:33 +0200

    Feat: added smaller screens support

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,17 +19,22 @@ export default function App() {
     <>
       <Header />
       <div
-        className="absolute inset-0 mt-24 w-full bg-dotted-spacing-8 bg-dotted-primary-100 sm:animate-pulse sm:bg-dotted-primary"
+        className="absolute inset-0 mt-20 hidden w-full bg-dotted-spacing-8 bg-dotted-primary-100 sm:block sm:animate-pulse sm:bg-dotted-primary"
         style={{ height: height }}
       />
       <div
-        className="container relative mx-auto bg-surface p-4 pt-24"
+        className="container relative mx-auto bg-surface p-4 lg:pt-24"
         ref={ref}
       >
-        <div className="relative flex">
-          <div className="w-1/2 space-y-8 p-2">
-            <h1 className="h2 text-primary-accent">Your Next Game!</h1>
-            <p className="h6">
+        <div className="relative lg:flex">
+          <img
+            src={catalogue}
+            alt=""
+            className="lg:clip-path-slash mb-6 h-64 w-full object-cover lg:absolute lg:right-0 lg:h-[330px] lg:w-[45vw] lg:rounded-xl xl:h-[380px]"
+          />
+          <div className="space-y-8 p-2 lg:w-1/2">
+            <h1 className="h4 sm:h2 text-primary-accent">Your Next Game!</h1>
+            <p className="s1 sm:h6">
               Your Next Game is a website that helps you find the next game to
               play based on your previous liked games. You tell us which game
               you like and which you don't and we'll suggest you the very next
@@ -37,22 +42,19 @@ export default function App() {
             </p>
             <PrimaryButton className="h6">Let's start!</PrimaryButton>
           </div>
-          <img
-            src={catalogue}
-            alt=""
-            className="clip-path-slash absolute right-0 h-[380px] w-[45vw] object-cover"
-          />
         </div>
-        <div className="relative mt-64 flex">
+        <div className="relative mt-16 lg:mt-64 lg:flex">
           <img
             src={rating}
             alt=""
-            className="clip-path-slash-reverse absolute left-0 h-[330px] w-[40vw] object-cover"
+            className="lg:clip-path-slash-reverse left-0 mb-6 h-64 w-full object-cover lg:absolute lg:h-[370px] lg:w-[40vw] lg:rounded-xl xl:h-[330px]"
           />
-          <div className="w-1/2" />
-          <div className="w-1/2 space-y-8 p-2">
-            <h2 className="h2 text-primary-accent">Tell Us What You Like!</h2>
-            <p className="h6">
+          <div className="lg:w-1/2" />
+          <div className="w-full space-y-8 p-2 lg:w-1/2">
+            <h2 className="h4 sm:h2 text-primary-accent">
+              Tell Us What You Like!
+            </h2>
+            <p className="s1 sm:h6">
               The first step to know what will be your favorite game is to tell
               up what games you like! All you need to do is rate some games on a
               scale of 1 to 5 and we'll do the hard work, and recommend you
@@ -61,21 +63,23 @@ export default function App() {
             <PrimaryButton>Start Rating</PrimaryButton>
           </div>
         </div>
-        <div className="relative mt-64 flex">
-          <div className="w-1/2 space-y-8 p-2">
-            <h2 className="h2 text-primary-accent">Get Recommendations!</h2>
-            <p className="h6">
+        <div className="relative mt-16 lg:mt-64 lg:flex">
+          <img
+            src={recommendations}
+            alt=""
+            className="lg:clip-path-slash right-0 mb-6 h-64 w-full object-cover lg:absolute lg:h-[350px] lg:w-[40vw] lg:rounded-xl xl:h-[280px]"
+          />
+          <div className="space-y-8 p-2 lg:w-1/2">
+            <h2 className="h4 sm:h2 text-primary-accent">
+              Get Recommendations!
+            </h2>
+            <p className="s1 sm:h6">
               After rating some games, you are only one click away from getting
               recommendations! Simply click the button below to view all the
               games that fits your unique and original taste!
             </p>
             <PrimaryButton>Recommendations</PrimaryButton>
           </div>
-          <img
-            src={recommendations}
-            alt=""
-            className="clip-path-slash absolute right-0 h-[280px] w-[40vw] object-cover"
-          />
         </div>
       </div>
     </>
