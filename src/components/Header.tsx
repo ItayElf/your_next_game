e4e872c8ce2209229e074f4481fdca8c60af7073
ItayElf <thh123321@gmail.com>
2023-02-03 15:02:21 +0200

    Feat: made header better in mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { ReactComponent as Logo } from "../assets/NextGameLogo.svg";
+import { ReactComponent as Icon } from "../assets/NextGameIcon.svg";
 import { getUser, isLoggedIn, logOutUser } from "../Auth";
 import PrimaryButton from "./theme/PrimaryButton";
 
@@ -17,19 +18,29 @@ export default function Header() {
     <header className="flex h-16 w-full items-center justify-between bg-primary-accent p-4">
       <div>
         <Link to={"/"}>
-          <Logo className="h-12 fill-black" />
+          <Logo className="hidden h-12 fill-black sm:inline" />
+          <Icon className="inline h-12 fill-black sm:hidden" />
         </Link>
       </div>
       <div>
         {isLogged ? (
-          <div className="flex items-center space-x-4 text-lg font-bold text-black">
+          <div className="flex items-center space-x-4 font-bold text-black sm:text-lg">
             <p className="block">Welcome, {user?.username}!</p>
-            <PrimaryButton onClick={onLogOut} className="bg-primary text-black">
+            <PrimaryButton
+              onClick={onLogOut}
+              className="hidden bg-primary text-black sm:inline"
+            >
               Log out
+            </PrimaryButton>
+            <PrimaryButton
+              onClick={onLogOut}
+              className="inline bg-primary !p-2 text-black sm:hidden"
+            >
+              <span className="material-symbols-outlined">logout</span>
             </PrimaryButton>
           </div>
         ) : (
-          <div className="space-x-4 text-lg font-bold">
+          <div className="space-x-4 font-bold sm:text-lg">
             <Link to={"/login"} className="text-black">
               Login
             </Link>
